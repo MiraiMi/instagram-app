f2c1f0a29ca59e13d7c4f5ca448c59a07f1f353d
MiraiMi <miraimirlan@gmail.com>
2023-02-20 01:49:50 +0600

    Finished and Provided the App and use contexts

---
+++ src/pages/layout/Layout.js
@@ -0,0 +1,35 @@
+
+
+import React from 'react'
+import { Components } from '../../components'
+import { Route, Routes as Switch } from 'react-router-dom'
+import { LayoutPages } from '../Lazy'
+import { Layout } from '../../services/path'
+import { Providers } from '../../providers'
+import { Hooks } from '../../hooks'
+
+export default function Layouts() {
+    const { token } = Providers.useAuth()
+    const { goToLogin } = Hooks.useLocations()
+
+    React.useEffect(() => {
+        if(!token){
+            goToLogin()
+        }
+    }, [token])
+
+    return (
+        <React.Fragment>
+            <section className='layout_row'>
+                <div className='layout_sidebar'>
+                    <Components.Sidebar />
+                </div>
+                <div className='layout_routes'>
+                    <Switch>
+                        <Route path={Layout.home} element={<LayoutPages.Home/>}/>
+                    </Switch>
+                </div>
+            </section>
+        </React.Fragment>
+    )
+}
